1ac47d7bb72c6727e0077be014cbab84567b97c6
wonbin <vlt8020@hurom.com>
2025-12-19 17:09:56 +0900

    Update

--- quick-upload.bat
+++ quick-upload.bat
@@ -1,10 +1,11 @@
 @echo off
-echo GitHub 빠른 업로드 중...
+chcp 65001 >nul
+echo GitHub Quick Upload...
 
 git add .
-git commit -m "자동 업데이트: %date% %time%"
+git commit -m "Auto update: %date% %time%"
 git push origin main
 
-echo 업로드 완료!
+echo Upload Complete!
 echo https://github.com/HanaBean/CompanyChat
 pause
---
+++ upload.bat
@@ -0,0 +1,6 @@
+@echo off
+git add .
+git commit -m "Update"
+git push origin main
+echo Done! Check: https://github.com/HanaBean/CompanyChat
+pause
